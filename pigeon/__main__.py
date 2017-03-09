bd06f9085e3476f57ef5392263af44a203e0f8eb
Patrick White <pfwhite@ufl.edu>
2017-03-09 16:32:09 -0500

    redcap does bad things
    When importing too many records(???) it decides to say that you dont
    have permissions to do this and seems to always spit the error up
    in xml. This is on version 6.16.0

--- pigeon/__main__.py
+++ pigeon/__main__.py
@@ -24,25 +24,20 @@ _output = '--output'
 _cv = 'cappy_version'
 _tk = 'token'
 _ru = 'redcap_url'
+_bs = 'batch_size'
 
-def main(args=docopt(docstr)):
-    with open(args[_config], 'r') as config_file:
-        global config
-        config = yaml.load(config_file.read())
-    with open(args[_file], 'r') as infile:
-        records_json = infile.read()
-    api = cappy.API(config[_tk], config[_ru], config[_cv])
+def batchify(records, batch_size=0):
+    if batch_size:
+        batched = [[]]
+        curr_batch = 0
+        for record in records:
+            if len(batched[curr_batch]) >= batch_size:
+                batched.append([])
+                curr_batch += 1
+            batched[curr_batch].append(record)
+    return batched
 
-    report = {
-        'num_subjects_uploaded': 0,
-        'subjects_uploaded': [],
-        'errors': [],
-        'datapoints_updated': 0,
-        'start_time': datetime.datetime.now().strftime("%Y-%m-%d %H:%M"),
-        'end_time': None
-    }
-
-    records = json.loads(records_json)
+def upload(records, report):
     response = api.import_records(data=records_json)
     data = json.loads(response.content)
     if data.get('error'):
@@ -66,7 +61,42 @@ def main(args=docopt(docstr)):
         for key in subj_ev:
             report['datapoints_updated'] += 1
 
-    print(json.dumps(report, indent=4))
+def main(args=docopt(docstr)):
+    with open(args[_config], 'r') as config_file:
+        global config
+        config = yaml.load(config_file.read())
+    with open(args[_file], 'r') as infile:
+        records_json = infile.read()
+    api = cappy.API(config[_tk], config[_ru], config[_cv])
+
+    reports = [{
+        'num_subjects_uploaded': 0,
+        'subjects_uploaded': [],
+        'errors': [],
+        'datapoints_updated': 0,
+        'start_time': datetime.datetime.now().strftime("%Y-%m-%d %H:%M"),
+        'end_time': None
+    }]
+
+    records = json.loads(records_json)
+    batched = [records]
+    response = api.import_records(data=records_json)
+
+    if response.status_code != 403:
+        data = json.loads(response.content)
+    else:
+        print('This is redcap\'s way of saying that there are too many records')
+        print(response)
+        print(response.content)
+        batched = batchify(records, config[_bs])
+
+    batch_reports = [copy(reports[0]) for item in range(0,len(batched))]
+
+    zipped = zip(batched, batch_reports)
+    for pair in zipped:
+        upload(pair[0], pair[1])
+
+    print(json.dumps(batch_reports, indent=4))
 
 def clean_err(err):
     return {
