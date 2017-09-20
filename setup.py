a29364c9b540c4d9124f7cbeda83fe94b0b72cdc
Matthew McConnell <bigmatt0211@ufl.edu>
2017-09-20 10:57:34 -0400

    made dependencies have specific version, and changed license

--- setup.py
+++ setup.py
@@ -6,12 +6,16 @@ setup(name='pigeon',
       url='http://github.com/pfwhite/pigeon',
       author='Patrick White',
       author_email='pfwhite9@gmail.com',
-      license='MIT',
+      license='Apache 2.0',
       packages=find_packages(),
       entry_points={
           'console_scripts': [
               'pigeon = pigeon.__main__:cli_run',
           ],
       },
-      install_requires=['cappy', 'docopt', 'pyyaml'],
+      install_requires=[
+          'cappy==1.1.1',
+          'docopt==0.6.2',
+          'pyyaml==3.12'],
+      dependency_links=["git+https://github.com/ctsit/cappy@1.1.1#egg=cappy-1.1.1"],
       zip_safe=False)
